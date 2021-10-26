0bfbaca52f7288eb17889e8144bd156272963a8a
demolitionok <51877153+demolitionok@users.noreply.github.com>
2021-10-26 23:09:45 +0200

    feat: set up ViewManager

--- Assets/Scripts/ViewManager.cs
+++ Assets/Scripts/ViewManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ViewManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private View _startingView;
+    [SerializeField]
+    private View[] _views;
+    
+    private Stack<View> _history;
+    private View _currentView;
+    
+    private T GetView<T>() where T : View
     {
-        
+        foreach (var view in _views)
+        {
+            if (view is T viewT)
+            {
+                return viewT;
+            }
+        }
+        Debug.Log($"Corresponding view was not found: {typeof(T)}");
+        return null;
+    }
+    
+    private void ShowView(View viewToShow, bool remember = true)
+    {
+        if (_currentView != null)
+        {
+            if (remember)
+            {
+                _history.Push(_currentView);
+            }
+            _currentView.Hide();
+        }
+        _currentView = viewToShow;
+        if(viewToShow != null)
+            viewToShow.Show();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowView<T>(bool remember = true) where T : View => ShowView(GetView<T>());
+
+    private void Start()
     {
-        
+        foreach (var view in _views)
+            view.Init();
+        ShowView(_startingView);
     }
 }
