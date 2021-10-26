f24bf353c462b6d535a15a202bba6874c4ed9090
demolitionok <51877153+demolitionok@users.noreply.github.com>
2021-10-26 21:41:08 +0200

    feat: set up an abstract View class

--- Assets/Scripts/View.cs
+++ Assets/Scripts/View.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class View : MonoBehaviour
+public abstract class View : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Init();
+    public void Hide() => gameObject.SetActive(false);
+    public void Show() => gameObject.SetActive(true);
 }
