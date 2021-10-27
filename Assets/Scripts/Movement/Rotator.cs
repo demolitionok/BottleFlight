2d82aa2ce9bdb6530d5dc20dc3bbd91aeedb6cdb
demolitionok <51877153+demolitionok@users.noreply.github.com>
2021-10-27 14:51:54 +0200

    fix: add new movement

--- Assets/Scripts/Movement/Rotator.cs
+++ Assets/Scripts/Movement/Rotator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,16 @@ using UnityEngine.InputSystem;
 
 public class Rotator : MonoBehaviour
 {
+    private Vector3 _vectorToRotate = Vector3.zero;
     public void OnJoystick(InputAction.CallbackContext ctx)
     {
         Vector2 val = ctx.ReadValue<Vector2>();
-        Vector3 rotateVector = new Vector3(val.x, val.y, 0);
-        gameObject.transform.Rotate(rotateVector);
+        Vector3 rotateVector = new Vector3(val.y, val.x, 0).normalized; 
+        _vectorToRotate = (Vector3.forward + rotateVector).normalized;
+    }
+
+    public void FixedUpdate()
+    {
+        gameObject.transform.Rotate(_vectorToRotate);
     }
 }
